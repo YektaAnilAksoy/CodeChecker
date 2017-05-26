d487a00c5cdb17f232e34b513380fb3cab413d04
Yekta Anıl <YektaAnilAksoy@users.noreply.github.com>
2017-05-26 16:55:42 +0300

    Update Compiler.java

--- src/main/java/org/anil/CodeChecker/process/Compiler.java
+++ src/main/java/org/anil/CodeChecker/process/Compiler.java
@@ -24,13 +24,28 @@ public class Compiler {
         String result="success";
 
         System.out.println("Code compilation started...");
-
-
+        
+        String filename=null;
         if(lang.equals("java")){
             System.out.println("java code is being compiled now ...");
-
+            filename="Solution.class";
             p = new ProcessBuilder("javac","Solution.java");
         }
+        else if(lang.equals("c")){
+            System.out.println("c code is being compiled now ...");
+            filename="a.out";
+            p = new ProcessBuilder("gcc","Solution.c");
+        }
+        else if(lang.equals("c++")){
+            System.out.println("c++ code is being compiled now ...");
+            filename="a.out";
+            p = new ProcessBuilder("g++","Solution.cpp");
+        }
+        /*else if(lang.equals("python")){
+            System.out.println("python code is being compiled now ...");
+            filename="Solution.py";
+            p = new ProcessBuilder("gcc????????","Solution.py");
+        }*/
         else{
             System.out.println("YANLIS LANGUAGE SECILMIS");
             p = new ProcessBuilder();
@@ -47,18 +62,29 @@ public class Compiler {
             InputStream is = pp.getInputStream();
             String temp;
             String error="";
+            boolean checkflag=false;
             try(BufferedReader b = new BufferedReader(new InputStreamReader(is))){
 
                 // error degiskenine error ciktisi ekleniyor ve response modeline ekleniyor.
                 while((temp = b.readLine()) != null){
-                    compiled = false;
+                	checkflag = true;
                     System.out.println(temp);
                     error += temp;
-                    result = error;
+                    
                 }
+                //compilationdan sonra warning mi hata mı oldugunu anlamak için o klasörde dosyanın yaratılıp yaratılmadıgını check ediyorum
+                //eger yaratılmassa warningdir ,yaratılmamıssa errordur.
 
-
-
+                File checkcompilation = new File(path+filename);
+                
+                if((checkcompilation.exists())){
+                	System.out.println("checked true, compiled true !");
+                	compiled = true;
+                }else{
+                	result = error;
+                }
+                
+                
                 //usteki kisim isini bitirene kadar blockluyor processi
                 pp.waitFor();
             }
